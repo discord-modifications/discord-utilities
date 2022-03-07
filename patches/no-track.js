4d667c5c54280ff2e8cbeb9340e67d82da8041d7
eternal <eternal.git@gmail.com>
2022-03-07 18:02:23 +0000

    disable additional tracking even if sentry isnt present

--- patches/no-track.js
+++ patches/no-track.js
@@ -6,15 +6,17 @@ export default {
    default: true,
    executor: async () => {
       await Webpack.whenReady;
-      if (window.__SENTRY__ === void 0) return () => {};
 
-      const Analytics = Webpack.findByProps('getSuperPropertiesBase64');
-      const Reporter = Webpack.findByProps('submitLiveCrashReport');
-      const Handlers = Webpack.findByProps('analyticsTrackingStoreMaker');
-      const Sentry = {
-         main: window.__SENTRY__.hub,
-         client: window.__SENTRY__.hub.getClient()
-      };
+      const [
+         Analytics,
+         Reporter,
+         Handlers
+      ] = Webpack.findByProps(
+         ['getSuperPropertiesBase64'],
+         ['submitLiveCrashReport'],
+         ['analyticsTrackingStoreMaker'],
+         { bulk: true }
+      );
 
       Analytics.__oldTrack = Analytics.track;
       Analytics.track = () => void 0;
@@ -25,30 +27,40 @@ export default {
       Reporter.__oldSubmitLiveCrashReport = Reporter.submitLiveCrashReport;
       Reporter.submitLiveCrashReport = () => void 0;
 
-      Sentry.client.close();
-      Sentry.main.getStackTop().scope.clear();
-      Sentry.main.getScope().clear();
-      Sentry.main.__oldAddBreadcrumb = Sentry.main.addBreadcrumb;
-      Sentry.main.addBreadcrumb = () => void 0;
+      if (window.__SENTRY__) {
+         const Sentry = {
+            main: window.__SENTRY__.hub,
+            client: window.__SENTRY__.hub.getClient()
+         };
 
-      window.__oldConsole = window.console;
-      Object.assign(window.console, ['debug', 'info', 'warn', 'error', 'log', 'assert'].forEach(
-         (method) => {
-            if (window.console[method].__sentry_original__) {
-               window.console[method] = window.console[method].__sentry_original__;
-            } else if (window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__) {
-               window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__ = window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__.__sentry_original__;
-            }
-         })
-      );
+         Sentry.client.close();
+         Sentry.main.getStackTop().scope.clear();
+         Sentry.main.getScope().clear();
+         Sentry.main.__oldAddBreadcrumb = Sentry.main.addBreadcrumb;
+         Sentry.main.addBreadcrumb = () => void 0;
+
+         window.__oldConsole = window.console;
+         Object.assign(window.console, ['debug', 'info', 'warn', 'error', 'log', 'assert'].forEach(
+            (method) => {
+               if (window.console[method].__sentry_original__) {
+                  window.console[method] = window.console[method].__sentry_original__;
+               } else if (window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__) {
+                  window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__ = window.console[method].__REACT_DEVTOOLS_ORIGINAL_METHOD__.__sentry_original__;
+               }
+            })
+         );
+      }
 
       return () => {
          Analytics.track = Analytics.__oldTrack;
          Reporter.submitLiveCrashReport = Reporter.__oldSubmitLiveCrashReport;
-         Sentry.main.addBreadcrumb = Sentry.main.__oldAddBreadcrumb;
-         Sentry.client.getOptions().enabled = true;
          Handlers.AnalyticsActionHandlers.handleTrack = Handlers.AnalyticsActionHandlers._handleTrack;
-         window.console = window.__oldConsole;
+
+         if (window.__SENTRY__) {
+            Sentry.main.addBreadcrumb = Sentry.main.__oldAddBreadcrumb;
+            Sentry.client.getOptions().enabled = true;
+            window.console = window.__oldConsole;
+         }
       };
    }
 };
